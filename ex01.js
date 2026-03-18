9bb8e592a5343124f3acc342b2fb49ef633c0e4f
GabrielaHDias <gabriela_h_dias@estudante.sesisenai.org.br>
2026-03-18 12:52:38 -0300

    add

---
+++ ex01.js
@@ -0,0 +1,10 @@
+function somarNumeros() {
+    let soma=0;
+    let continuar = true;
+    while <continuar) {
+        let numero = parseFloat(prompt(Digite um número para somar: "));
+            if (!isNaN(numero)) {
+                soma += numero;
+            }
+    }
+}
