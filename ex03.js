7da97c288baf5b52e005998bf5e28f8e1ccb37a5
GabrielaHDias <gabriela_h_dias@estudante.sesisenai.org.br>
2026-03-25 12:45:06 -0300

    add

--- ex03.js
+++ ex03.js
@@ -5,7 +5,7 @@ function empresaMawer() {
     for(let i = 1; 1<13; i++){
         let ganho = parseFloat(prompt("Ganho da empresa no mês: "+ i));
         let gasto = parseFloat(prompt("Gasto da empresa no mês: "+ i));
-        TotalGanho += ganho;
-        TotalGasto += gasto;
+        totalGanho += ganho;
+        totalGasto += gasto;
     }
 }
